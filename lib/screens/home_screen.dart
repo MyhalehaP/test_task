eff72908732a47e0a5194d86dbabb28a63df89ed
Peter <migalega.p@gmail.com>
2020-10-15 17:36:15 +0300

    minor: removed debug banner

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,18 +4,16 @@ import 'package:test_task/components/greeting_text.dart';
 import 'package:test_task/components/screen_color.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-              body: Stack(
-                  children: [
-                    ScreenColor(),
-                    GreetingText(),
-                  ]),
-            ),
-        );
+      home: Scaffold(
+        body: Stack(children: [
+          ScreenColor(),
+          GreetingText(),
+        ]),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
-
 }
